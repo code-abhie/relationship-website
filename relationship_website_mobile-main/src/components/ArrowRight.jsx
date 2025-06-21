79c7875708cfc62527ca901c72b579a757209c22
code-abhie <abhie041204@gmail.com>
2025-06-21 22:51:25 +0530

    Add files via upload

---
+++ relationship_website_mobile-main/src/components/ArrowRight.jsx
@@ -0,0 +1,9 @@
+const ArrowRight = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M5 12h14M12 5l7 7-7 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
+
+export default ArrowRight;
